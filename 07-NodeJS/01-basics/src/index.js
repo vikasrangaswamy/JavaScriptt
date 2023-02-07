d6c88fd3ef1072ddb2c819fd2a84b62bd1b34506
Vikas Rangaswamy <71181819+vikasrangaswamy@users.noreply.github.com>
2023-02-07 12:40:10 +0530

    installed Axios

--- 07-NodeJS/01-basics/src/index.js
+++ 07-NodeJS/01-basics/src/index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const fs = require('fs');
+
+fs.readFile('../data/names.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
