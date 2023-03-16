7714f6f50cca4afcfcb235c10c5ce913e3c4d6ee
Vikas Rangaswamy <71181819+vikasrangaswamy@users.noreply.github.com>
2023-03-16 19:43:15 +0530

    working on marketplace apps

--- 16-typescript/dist/index.js
+++ 16-typescript/dist/index.js
@@ -1,4 +1,17 @@
 "use strict";
 console.log("TypeScript");
 let age = 20;
+let myFunction = (params) => {
+    console.log("myFunction", params);
+};
+let arrayFunction = (param) => {
+    console.log("parameters passed = ", param);
+    let numberArray;
+    let alphaNumericArray;
+    numberArray = [1, 2, 3, 4, 5, 6];
+    console.log("number array : ", numberArray);
+    alphaNumericArray = [1, "vikas"];
+    console.log("alpha numeric array : ", alphaNumericArray);
+};
+arrayFunction("no parameter");
 //# sourceMappingURL=index.js.map
--- 16-typescript/src/index.ts
+++ 16-typescript/src/index.ts
@@ -1,3 +1,25 @@
 console.log("TypeScript")
 
 let age : number = 20
+
+let myFunction = (params: any)=>{
+    console.log("myFunction" , params)
+}
+
+// myFunction("hello")
+
+let arrayFunction = (param:any)=>{
+    console.log("parameters passed : " , param)
+    let numberArray :number[]
+    let alphaNumericArray : [number,string]
+
+    numberArray=[1,2,3,4,5,6]
+    console.log("number array : " , numberArray)
+
+    alphaNumericArray = [1,"vikas"];
+
+    console.log("alpha numeric array : " , alphaNumericArray)
+}
+
+// arrayFunction("no parameter")
+
